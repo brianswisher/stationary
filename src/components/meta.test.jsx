272a55eabc776346735b3c978dbcb4197b37dccc
Brian Swisher <brian.swisher@syapse.com>
2019-08-03 16:59:46 -0700

    jest snapshot tests

---
+++ src/components/meta.test.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Meta from './meta';
+
+const data = ['a', 'b']
+
+describe("Tags", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Meta popularity={4} reliability={80} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ src/components/tags.test.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Tags from './tags';
+
+const data = ['a', 'b']
+
+describe("Tags", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Tags data={data} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
